f37318757b5391eafbefbd0778df070694a07ca7
Victoria <bernard.victoria22@gmail.com>
2019-04-24 20:55:26 -0500

    Make static site pretty

--- static-site.js
+++ static-site.js
@@ -1,47 +1,74 @@
-/** 
- * serveRawHtml delievers a response with HTML inputted directly into the worker script
-*/
-async function serveRawHtml(request) {
-    const someHTML = `
-   <!DOCTYPE html>
-    <html>
-    <body>
+const someHTML = `
+<!DOCTYPE html>
+<html>
+<body>
 
-    <h1>Hello World</h1>
-    <p>This is all generated using a Worker</p>
-     <iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<h1>Hello World</h1>
+<p>This is all generated using a Worker</p>
+<iframe
+    width="560"
+    height="315"
+    src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+    frameborder="0"
+    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+    allowfullscreen
+></iframe>
 
-    </body>
-    </html>
-    `
+</body>
+</html>
+`
+
+/**
+ * rawHtmlResponse delievers a response with HTML inputted directly
+ * into the worker script
+ * @param {string} html 
+ */
+async function rawHtmlResponse(html) {
     const init = {
         headers: {
             'content-type': 'text/html;charset=UTF-8'
         }
     }
-    return new Response(someHTML, init)
+
+    return new Response(html, init)
 }
+
+//TODO add simple make subrequest
+
 /** 
- * serveFromCloudStorage takes in a request from the path /cloud-storage/someresource 
- * and then fetches someresource or /index.html from a digital ocean space
+ * TODO changeto use KV https://www.npmjs.com/package/mime-types
+ * cloudStorageResponse takes in a request from the url cloudBaseURL/someresource
+ * and then fetches someresource or defaultFilePath from that cloud storage
 */
-async function serveFromCloudStorage(request) {
+
+/**
+ * 
+ * @param {string} cloudBaseURL 
+ * @param {string} defaultFilePath 
+ */
+async function cloudStorageResponse(cloudBaseURL, defaultFilePath) {
     const parsedUrl = new URL(request.url)
-    let path = parsedUrl.pathname
-    path = parsedUrl.pathname.replace("/cloud-storage", "")
-    let lastSegment = path.substring(path.lastIndexOf('/'))
-    console.log(lastSegment)
+    const path = parsedUrl.pathname.replace("/cloud-storage", "")
+    const lastSegment = path.substring(path.lastIndexOf('/'))
+
     if (lastSegment.indexOf('.') === -1) {
-        path += '/index.html'
+        path += defaultFilePath
     }
-    console.log(path)
-    return fetch("https://cloudflare-example-space.nyc3.digitaloceanspaces.com" + path)
+
+    return fetch(cloudBaseURL + path)
 }
 
 addEventListener('fetch', event => {
-    const url = event.request.url
+    const { url } = event.request
+
     // Replace with the routes you wish to serve static resources from 
-    if (url.includes('/cloud-storage')) return event.respondWith(serveFromCloudStorage(event.request))
-    if (url.endsWith('/raw')) return event.respondWith(serveRawHtml(event.request))
-    event.respondWith(new Response("not-found", { status: 404 }))
+    if (url.includes('/cloud-storage')) {
+        return event.respondWith(
+            cloudStorageResponse("https://cloudflare-example-space.nyc3.digitaloceanspaces.com", "index.html")
+        )
+    }
+
+    if (url.endsWith('/raw')) {
+        return event.respondWith(rawHtmlResponse(someHTML))
+    }
 })
